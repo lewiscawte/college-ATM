5612342cfbfe5a38b6db280a5e9ea5c8a6a58c21
lewiscawte <lewis@lewiscawte.me>
2014-01-28 22:46:35 +0000

    Rename Started Menu.c to menu.c. Start cleaning up menu.c: * Remove unused libraries * Rename function. * Renumber options. * Line breaks, spacing. * Close function.

---
+++ menu.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int menu() {
+    printf("Welcome to the Brokemans ATM\n-------------------------");
+    printf("\tTip of the Day: Remember if you want to see your money spent in a flash let your wife know your card details!\n\n");
+    printf("\tWhat would you like to do?\n");
+    printf("\t1. Display Balance\n");
+    printf("\t2. Print Balance\n");
+    printf("\t3. Cash With Receipt\n");
+    printf("\t4. Cash Without Receipt\n");
+    printf("\t5. Order Statement\n");
+    printf("\t6. Order Chequebook\n");
+    printf("\t7. Return Card\n");    	
+}
+
+
+
