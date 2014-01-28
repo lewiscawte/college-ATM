f7a554c04a61371ce073670201e664d314a3aab9
lewiscawte <lewis@lewiscawte.me>
2014-01-28 23:15:53 +0000

    Add selection code to menu.c.

--- menu.c
+++ menu.c
@@ -4,15 +4,62 @@
 #include<stdlib.h>
 
 int menu() {
-	printf("Welcome to the Brokemans ATM\n-------------------------");
-	printf("Tip of the Day: Remember - If you want to see your money spent in a flash, let your wife know your card details!\n\n");
-	printf("What would you like to do?\n");
-	printf("\t1. Balance\n");
-	printf("\t2. Withdrawal\n");
-	printf("\t3. Change PIN\n");
-	printf("\t4. Deposit\n");
-	printf("\t5. Transfer Funds\n");
-	printf("\t6. Order Statement\n");
-	printf("\t7. Order Chequebook\n");
-	printf("\t8. Exit\n");
+	int exit = 0; // Exit loop (when set to 1, program exits)
+	
+	while( exit == 0 ) {
+		printf("Welcome to the Brokemans ATM\n-------------------------");
+		printf("Tip of the Day: Remember - If you want to see your money spent in a flash, let your wife know your card details!\n\n");
+		printf("What would you like to do?\n");
+		// BEGIN: Menu Options
+		printf("\t1. Balance\n");
+		printf("\t2. Withdrawal\n");
+		printf("\t3. Change PIN\n");
+		printf("\t4. Deposit\n");
+		printf("\t5. Transfer Funds\n");
+		printf("\t6. Order Statement\n");
+		printf("\t7. Order Chequebook\n");
+		printf("\t8. Exit\n");
+		// END: Menu Options
+		
+		printf("Select an option: ");
+		fflush(stdin);
+		scanf("%d", &option);
+		if( option<1 || option>8 ) {
+			printf("Please enter a number between 1 and 8\n"); 
+		}
+		
+		switch(option) {
+			case 1:
+				checkBalance();
+			break;
+			case 2:
+				withdrawal();
+			break;
+			case 3:
+				changePIN();
+			break;
+			case 1337:
+				// Really cool easter egg...
+				printf("YOU ARE SO COOL!");
+			break;
+			case 4:
+				depositFunds();
+			break;
+			case 5:
+				transferFunds();
+			break;
+			case 6:
+				orderStatement();
+			break;
+			case 7:
+				orderChequebook;
+			break;
+			case 8:
+				exit = 1;
+			break;
+		}
+	}
+	
+	exitATM(); // Run exit function
+	
 }
