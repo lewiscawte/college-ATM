3d90456afcfd214ebf260698d7229f1c968031b6
lewiscawte <lewis@lewiscawte.me>
2014-01-28 22:51:08 +0000

    Add comments to menu.c and trip whitespace.

--- menu.c
+++ menu.c
@@ -1,18 +1,17 @@
+// Function to display a menu of options and allow user to access other parts of the system.
+// @author ??? <??.??13@smail.bhasvic.ac.uk>
 #include<stdio.h>
 #include<stdlib.h>
 
 int menu() {
     printf("Welcome to the Brokemans ATM\n-------------------------");
-    printf("\tTip of the Day: Remember if you want to see your money spent in a flash let your wife know your card details!\n\n");
-    printf("\tWhat would you like to do?\n");
+    printf("Tip of the Day: Remember if you want to see your money spent in a flash let your wife know your card details!\n\n");
+    printf("What would you like to do?\n");
     printf("\t1. Display Balance\n");
     printf("\t2. Print Balance\n");
     printf("\t3. Cash With Receipt\n");
     printf("\t4. Cash Without Receipt\n");
     printf("\t5. Order Statement\n");
     printf("\t6. Order Chequebook\n");
-    printf("\t7. Return Card\n");    	
+    printf("\t7. Return Card/Exit\n");    	
 }
-
-
-
