ade298c76ea8e846521718c10f006c85f6a21a75
lewiscawte <lewis@lewiscawte.me>
2014-02-05 01:06:04 +0000

    Indenting and typo fixing transferFunds.c.

--- transferFunds.c
+++ transferFunds.c
@@ -1,16 +1,16 @@
 // Function to transfer funds to another account.
-// @author Lewis Cawte <lewi@lewiscawte.me>
+// @author Lewis Cawte <lewis@lewiscawte.me>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
 void transferFunds() {
 
-	// Variables to match the globals, renamed to victim for recepient. 
+	// Variables to match the globals, renamed to victim for recipient. 
 	char victimPin[5]="", victimCustName[41]="", victimCardNumb[17]="";
 	float victimBalance=0, transferAmount=0;
 
-	// @term: victim - the person who is the recipeint
+	// @term: victim - the person who is the recipient
 	// Get the victim's account details.
 	printf("Please enter the account number you wish to transfer funds to.\n");
 	gets(victimCardNumb);
@@ -27,8 +27,8 @@ void transferFunds() {
 	fscanf(victimData, "%s %s %s %f", victimCustName, victimCardNumb, victimPin, &victimBalance);
 
 	do {
-	printf("Please enter the amount you would like to transfer to %s: " victimCustName);
-	scanf("%f", &transferAmount);
+		printf("Please enter the amount you would like to transfer to %s: " victimCustName);
+		scanf("%f", &transferAmount);
 	} while( transferAmount > balance || transferAmount < 0 );
-	printf("Transfering %.2f to %s (Acct. %s)", transferAmount, victimCustName, victimCardNumb).
+	printf("Transferring %.2f to %s (Acct. %s)", transferAmount, victimCustName, victimCardNumb).
 }
