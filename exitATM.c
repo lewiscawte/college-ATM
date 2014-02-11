88dc69cdb01a111fcd3433663c29f9c55149bcff
lewiscawte <lewis@lewiscawte.me>
2014-02-11 23:35:37 +0000

    Add exitATM.c - new function (with more TODO?) that shows log out data and writes stuff to a file.

---
+++ exitATM.c
@@ -0,0 +1,11 @@
+// Function to show log out messages and write data back to the file.
+// @author Lewis Cawte <lewis@lewiscawte.me>
+#include<stdin.h>
+#include<stdlib.h>
+
+void exitATM() {
+	writeCustData();
+	
+	printf("Thank you for using Brokemans ATM.");
+	printf("Please take your card.");
+}
