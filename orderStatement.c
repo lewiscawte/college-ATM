601ac5f9ee0ee9e18cb9fa4b556bc4bce2e479c2
lewiscawte <lewis@lewiscawte.me>
2014-02-11 23:56:55 +0000

    Rework some of orderStatement.c (untested)

--- orderStatement.c
+++ orderStatement.c
@@ -1,34 +1,29 @@
-#include<stdio.h>
+// Function to show 10 most recent transactions, and post a statement.
+// @author David ??? <david.???13@smail.bhasvic.ac.uk>
+#include<stdin.h>
 #include<stdlib.h>
 #include<strings.h>
-int orderStatement() {
+void orderStatement() {
 	FILE *customerStatement;
-	char buffer[20], char statementFileLocation[101] = "";
+	char buffer[10][31], statementFileLocation[101] = "";
+	int counter=0;
 
 	strcat(statementFileLocation, "N:\\");
 	strcat(statementFileLocation, cardNumber );
 	strcat(statementFileLocation, "-transactions.txt"); 
 
-	FILE *customerStatement;
 	customerStatement = fopen(statementFileLocation, "r");
 	
-	fscanf(customerStatement, "%s", buffer);
-	fscanf(customerStatement, "%s", buffer);
-	fscanf(customerStatement, "%s", buffer);
-	fscanf(customerStatement, "%s", buffer);
-	fscanf(customerStatement, "%s", buffer);
-	fscanf(customerStatement, "%s", buffer);
-	fscanf(customerStatement, "%s", buffer);
-	fscanf(customerStatement, "%s", buffer);
-	fscanf(customerStatement, "%s", buffer);
-	fscanf(customerStatement, "%s", buffer);
-	fscanf(customerStatement, "%s", buffer);
-	fscanf(customerStatement, "%s", buffer);
-	fscanf(customerStatement, "%s", buffer);
-	fscanf(customerStatement, "%s", buffer);
-	fscanf(customerStatement, "%s", buffer);
+	for (counter=0;counter<9;counter++) {
+		fscanf(customerStatement, "%s", buffer[counter]);
+	}
 	
-	printf("Transactions: %s\n", buffer );
+	for (counter=0;counter<9;counter++) {
+		printf("Transactions: %s\n", buffer[counter] );
+	}
+	
+	printf( "\n\nStatement will be sent to the following address:\n%s\n", address );
+	
 	fclose(customerStatement);
 	
 	system("PAUSE");
