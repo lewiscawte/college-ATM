57d78fa540ab276f8f8af26eec221a339f8b27d6
lewiscawte <lewis@lewiscawte.me>
2014-02-11 23:57:53 +0000

    Add a new function, orderChequebook in orderChequebook.c. Pretty much a pointless function but it keeps menu.c clean.

---
+++ orderChequebook.c
@@ -0,0 +1,9 @@
+// Function to tell the user a chequebook has been sent to them.
+// @author Lewis Cawte <lewis@lewiscawte.me>
+#include<stdin.h>
+#include<stdlib.h>
+
+void orderStatement() {
+	// Very pointless function ftw.
+	printf( "\n\nStatement will be sent to the following address:\n%s\n", address );
+}
